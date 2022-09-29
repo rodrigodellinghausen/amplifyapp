7dfc85c945e986dda8074349f3e0cf640567ce34
Rodrigo Dellinghausen <rodrigo@wswork.com.br>
2022-09-29 18:57:42 -0300

    graphql and s3

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,13 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://5654j4urxbaa3lsprees6r3txy.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-gikszmdf45azhninhthhlsnxhq",
+    "aws_user_files_s3_bucket": "amplifyappbucket1122142-dev",
+    "aws_user_files_s3_bucket_region": "us-west-2"
 };
 
 
